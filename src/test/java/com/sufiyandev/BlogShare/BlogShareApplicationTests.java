17a7ca607c8e249683ac87a72cf563a6e305263e
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-03-27 17:25:06 +0530

    initialize the Share Blog Project
    Signed-off-by: Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>

---
+++ src/test/java/com/sufiyandev/BlogShare/BlogShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sufiyandev.BlogShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
